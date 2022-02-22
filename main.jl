cc436915a8015162395fbe969b2ca91cfa43d954
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-02-22 18:13:28 +0100

    added CPU gathering operation

--- main.jl
+++ main.jl
@@ -1,16 +1,14 @@
 using Statistics
-
-include("src/utils/Utils.jl")
-include("src/Scatter.jl")
+using StencilInterpolation
 
 # random particles
-N = 100000
+N = Int(1e6)
 px, py = rand(N), rand(N)
 particle_coords = (px, py)
 p = (px, py)
 
 # model domain
-nx = ny = 1001
+nx = ny = 101
 lx = ly = 1
 dx, dy = lx/(nx-1), ly/(ny-1)
 x = LinRange(0, lx, nx)
@@ -22,26 +20,32 @@ F = [-sin(2*yi)*cos(3*π*xi) for xi in x, yi in y]
 # Scattering operation (for now just bi-linear interpolation)
 @time Fp = scattering( (x, y), (dx, dy), F, particle_coords);
 
-# Compute error
-Fanalytic = [-sin(2*yi)*cos(3*π*xi) for (xi, yi) in zip(px,py)]
-misfit = @.(log10(abs(Fp-Fanalytic)))
-println("average log10 error = $(mean(misfit))")
+# # Compute error
+# Fanalytic = [-sin(2*yi)*cos(3*π*xi) for (xi, yi) in zip(px,py)]
+# misfit = @.(log10(abs(Fp-Fanalytic)))
+# println("average log10 error = $(mean(misfit))")
 
-# Plots
-f = Figure(resolution=(1600, 600))
-a = Axis(f[1,1], aspect=1, title="Analytical")
-heatmap!(a, x, y, F, colormap=:batlow)
-xlims!(0, lx)
-ylims!(0, ly)
+# # Plots
+# f = Figure(resolution=(1600, 600))
+# a = Axis(f[1,1], aspect=1, title="Analytical")
+# heatmap!(a, x, y, F, colormap=:batlow)
+# xlims!(0, lx)
+# ylims!(0, ly)
 
-a = Axis(f[1, 2], aspect=1, title="Scattered")
-scatter!(a, px, py, color=Fp, colormap=:batlow)
-xlims!(0, lx)
-ylims!(0, ly)
+# a = Axis(f[1, 2], aspect=1, title="Scattered")
+# scatter!(a, px, py, color=Fp, colormap=:batlow)
+# xlims!(0, lx)
+# ylims!(0, ly)
 
-a = Axis(f[1, 3], aspect=1, title="log10 error")
-s = scatter!(a, px, py, color=misfit, colormap=:batlow)
-xlims!(0, lx)
-ylims!(0, ly)
-Colorbar(f[1,4], s)
-display(f)
+# a = Axis(f[1, 3], aspect=1, title="log10 error")
+# s = scatter!(a, px, py, color=misfit, colormap=:batlow)
+# xlims!(0, lx)
+# ylims!(0, ly)
+# Colorbar(f[1,4], s)
+# display(f)
+
+# CUDA
+Fpd = CUDA.zeros(Float64, N)
+Fd = CuArray(F)
+
+@time scattering!(Fpd, (x, y), (dx, dy), Fd, CuArray.(particle_coords));
