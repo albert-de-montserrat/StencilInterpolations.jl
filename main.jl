e9b7fef80d1133492042386ac185bc44c9636929
albert-de-montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2022-02-22 11:09:42 +0200

    updates

--- main.jl
+++ main.jl
@@ -1,23 +1,47 @@
-using StaticArrays
+using Statistics
 
 include("src/utils/Utils.jl")
+include("src/Scatter.jl")
 
-nx = ny = 11
+# random particles
+N = 100000
+px, py = rand(N), rand(N)
+particle_coords = (px, py)
+p = (px, py)
+
+# model domain
+nx = ny = 1001
 lx = ly = 1
 dx, dy = lx/(nx-1), ly/(ny-1)
 x = LinRange(0, lx, nx)
 y = LinRange(0, ly, ny)
 
 # random field
-T = [rand() for _ in x, _ in y]
+F = [-sin(2*yi)*cos(3*π*xi) for xi in x, yi in y]
 
-N = 100000
-px, py = rand(N), rand(N)
-particle_coords = (px, py)
+# Scattering operation (for now just bi-linear interpolation)
+@time Fp = scattering( (x, y), (dx, dy), F, particle_coords);
 
-p = (px, py)
-dxi = (dx, dy)
+# Compute error
+Fanalytic = [-sin(2*yi)*cos(3*π*xi) for (xi, yi) in zip(px,py)]
+misfit = @.(log10(abs(Fp-Fanalytic)))
+println("average log10 error = $(mean(misfit))")
 
+# Plots
+f = Figure(resolution=(1600, 600))
+a = Axis(f[1,1], aspect=1, title="Analytical")
+heatmap!(a, x, y, F, colormap=:batlow)
+xlims!(0, lx)
+ylims!(0, ly)
 
-gather(xi, dxi, F, particle_coords)
-gather(xi, dxi, F, particle_coords)
+a = Axis(f[1, 2], aspect=1, title="Scattered")
+scatter!(a, px, py, color=Fp, colormap=:batlow)
+xlims!(0, lx)
+ylims!(0, ly)
+
+a = Axis(f[1, 3], aspect=1, title="log10 error")
+s = scatter!(a, px, py, color=misfit, colormap=:batlow)
+xlims!(0, lx)
+ylims!(0, ly)
+Colorbar(f[1,4], s)
+display(f)
