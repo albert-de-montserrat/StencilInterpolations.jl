06cc74c07418c57724a5bab02ad74659817cabab
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-07-21 11:47:29 +0200

    update

--- src/StencilInterpolations.jl
+++ src/StencilInterpolations.jl
@@ -11,7 +11,8 @@ include("kernels.jl")
 # include("trilinear.jl")
 
 export scattering, scattering!, gathering!
-export grid2particle, grid2particle!, grid2particle_xcell!, gathering_xcell!, _grid2particle_xcell_centered
+export _grid2particle, grid2particle, grid2particle!
+export grid2particle_xcell!, gathering_xcell!, _grid2particle_xcell_centered
 export lerp, ndlinear, random_particles
 export parent_cell
 
